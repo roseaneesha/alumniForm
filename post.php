c61503486d315bfefe59404a0a945f9b4a1973d3
roseaneesha <roseaneesha02@gmail.com>
2022-05-21 14:43:00 +0530

    backend done

--- post.php
+++ post.php
@@ -1,9 +1,15 @@
 <?php
 
+// header("Access-Control-Allow-Origin:http://localhost:4200");
+// header("Access-Control-Allow-Methods: POST ");
+// header("Access-Control-Allow-Headers:  Content-Type, Accept");
+
 header("Access-Control-Allow-Origin:*");
-header("Access-Control-Allow-Methods: PUT, GET, POST, OPTIONS ");
-header("Access-Control-Allow-Headers: Origin, X-Requested-Width, Content-
-Type, Accept");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,PUT, GET, POST, OPTIONS ");
+header('Access-Control-Max-Age: 1000');
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization, access-control-allow-credentials, access-control-allow-headers, access-control-allow-methods");
+// error_reporting(0);
 
 $connect = mysqli_connect('localhost', 'root', '', 'alumni') or die;
 if ($connect) {
@@ -14,10 +20,102 @@ if ($connect) {
 
 
 $alumniFeedback = file_get_contents("php://input");
-var_dump($alumniFeedback);
-if (isset($alumniFeedback) && !empty($alumniFeedback)) {
+// var_dump($alumniFeedback);
+if (isset($alumniFeedback)) {
     $req = json_decode($alumniFeedback);
-    var_dump($req);
+    // var_dump($req);
+    $stuName = mysqli_real_escape_string($connect, $req->stuName);
+
+    $orgName
+        = mysqli_real_escape_string($connect, $req->orgName);
+
+    $organization
+        =  implode(',', $req->organization);
+
+    $qualifications
+        =  implode(',', $req->quaifications);
+
+    $email
+        = mysqli_real_escape_string($connect, $req->email);
+
+    $mobile
+        = mysqli_real_escape_string($connect, $req->mobile);
+
+    $regNum
+        = mysqli_real_escape_string($connect, $req->regNum);
+
+    $q1
+        = $req->q1;
+
+    $q2
+        = $req->q2;
+
+    $q3
+        = $req->q3;
+
+    $q4
+        = $req->q4;
+
+    $q5
+        = $req->q5;
+
+    $q6
+        = $req->q6;
+
+    $q7
+        = $req->q7;
+
+    $q8
+        = $req->q8;
+
+    $q9
+        = $req->q9;
+
+    $q10
+        = $req->q10;
+
+    $q11
+        = $req->q11;
+
+    $q12
+        = $req->q12;
+
+    $q13
+        = $req->q13;
+
+    $q14
+        = $req->q14;
+
+    $q14b
+        = mysqli_real_escape_string($connect, $req->q14b);
+
+    $q15
+        = $req->q15;
+
+    $q15b
+        = mysqli_real_escape_string($connect, $req->q15b);
+
+    $q16
+        = $req->q16;
+
+    $q17
+        = $req->q17;
+
+    $q18
+        = mysqli_real_escape_string($connect, $req->q18);
+
+
+
+
+    // $insertQuery = "INSERT INTO feedback (studentName,orgName, organization,qualification,email,mobile,regNum,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q14b,q15,q15b,q16,q17,q18) VALUES ('$stuName','$orgName','$organization','$qualifications','$email','$mobile','$regNum','$q1','$q2','$q3','$q4','$q5','$q6','$q7','$q8','$q9','$q10','$q11','$q12','$q13','$q14','$q14b',$q15','$q16','$q17','$q18')";
+    $insertQuery = "INSERT INTO feedback (studentName,orgName,organization,qualification,email,mobile,regNum,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q14b,q15,q15b,q16,q17,q18) VALUES ('$stuName','$orgName','$organization','$qualifications','$email','$mobile','$regNum','$q1','$q2','$q3','$q4','$q5','$q6','$q7','$q8','$q9','$q10','$q11','$q12','$q13','$q14','$q14b','$q15','$q15b','$q16','$q17','$q18')";
+    $check = mysqli_query($connect, $insertQuery);
+
+    if (!$check) {
+        echo (mysqli_error($connect));
+    } else {
+        echo 'done';
+    }
 } else {
     echo "error occured";
 }
