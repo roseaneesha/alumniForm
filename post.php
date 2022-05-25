784dbe7afadb2e0ff210244b8d930162a7b731ce
roseaneesha <roseaneesha02@gmail.com>
2022-05-25 14:48:19 +0530

    fixed cors error

--- post.php
+++ post.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: GET,PUT, GET, POST, OPTIONS ");
 header('Access-Control-Max-Age: 1000');
 header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization, access-control-allow-credentials, access-control-allow-headers, access-control-allow-methods");
-// error_reporting(0);
+error_reporting(0);
 
 
 $connect = mysqli_connect('localhost', 'root', '', 'alumni') or die;
@@ -16,40 +16,40 @@ if ($connect) {
 }
 
 
-$alumniFeedback = file_get_contents("php://input");
+$alumniFeedback = json_decode(file_get_contents("php://input"));
 
 if (isset($alumniFeedback) && !empty($alumniFeedback)) {
-    $req = json_decode($alumniFeedback);
 
 
 
-    $stuName = mysqli_real_escape_string($connect, $req->stuName);
-    $orgName = mysqli_real_escape_string($connect, $req->orgName);
-    $organization = implode(',', $req->organization);
-    $qualifications = mysqli_real_escape_string($connect, $req->qualifications);
-    $email = mysqli_real_escape_string($connect, $req->email);
-    $mobile = mysqli_real_escape_string($connect, $req->mobile);
-    $regNum = mysqli_real_escape_string($connect, $req->regNum);
-    $q1 = $req->q1;
-    $q2 = $req->q2;
-    $q3 = $req->q3;
-    $q4 = $req->q4;
-    $q5 = $req->q5;
-    $q6 = $req->q6;
-    $q7 = $req->q7;
-    $q8 = $req->q8;
-    $q9 = $req->q9;
-    $q10 = $req->q10;
-    $q11 = $req->q11;
-    $q12 = $req->q12;
-    $q13 = $req->q13;
-    $q14 = $req->q14;
-    $q14b = mysqli_real_escape_string($connect, $req->q14b);
-    $q15 = $req->q15;
-    $q15b = mysqli_real_escape_string($connect, $req->q15b);
-    $q16 = $req->q16;
-    $q17 = $req->q17;
-    $q18 = mysqli_real_escape_string($connect, $req->q18);
+
+    $stuName = mysqli_real_escape_string($connect, $alumniFeedback->stuName);
+    $orgName = mysqli_real_escape_string($connect, $alumniFeedback->orgName);
+    $organization = implode(',', $alumniFeedback->organization);
+    $qualifications = mysqli_real_escape_string($connect, $alumniFeedback->qualifications);
+    $email = mysqli_real_escape_string($connect, $alumniFeedback->email);
+    $mobile = mysqli_real_escape_string($connect, $alumniFeedback->mobile);
+    $regNum = mysqli_real_escape_string($connect, $alumniFeedback->regNum);
+    $q1 = $alumniFeedback->q1;
+    $q2 = $alumniFeedback->q2;
+    $q3 = $alumniFeedback->q3;
+    $q4 = $alumniFeedback->q4;
+    $q5 = $alumniFeedback->q5;
+    $q6 = $alumniFeedback->q6;
+    $q7 = $alumniFeedback->q7;
+    $q8 = $alumniFeedback->q8;
+    $q9 = $alumniFeedback->q9;
+    $q10 = $alumniFeedback->q10;
+    $q11 = $alumniFeedback->q11;
+    $q12 = $alumniFeedback->q12;
+    $q13 = $alumniFeedback->q13;
+    $q14 = $alumniFeedback->q14;
+    $q14b = mysqli_real_escape_string($connect, $alumniFeedback->q14b);
+    $q15 = $alumniFeedback->q15;
+    $q15b = mysqli_real_escape_string($connect, $alumniFeedback->q15b);
+    $q16 = $alumniFeedback->q16;
+    $q17 = $alumniFeedback->q17;
+    $q18 = mysqli_real_escape_string($connect, $alumniFeedback->q18);
 
 
 
@@ -61,8 +61,6 @@ if (isset($alumniFeedback) && !empty($alumniFeedback)) {
     } else {
 
         echo '\ndone';
-
-
     }
 } else {
     echo "\nerror occured";
